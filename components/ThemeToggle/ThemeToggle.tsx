842274bbda2701cba315bd3a8cc6d8fb3b622097
Surdu <neon-doubt@maskmail.net>
2022-08-20 02:02:12 +0300

    Initial work for theme switcher

--- components/ThemeToggle/ThemeToggle.tsx
+++ components/ThemeToggle/ThemeToggle.tsx
@@ -1,7 +1,22 @@
 import useDarkTheme from "~/components/ThemeToggle/useDarkTheme";
 
+import styles from "./ThemeToggle.module.scss";
+
 export default function ThemeToggle() {
   const { darkTheme, toggleDarkMode } = useDarkTheme();
 
-  return <div onClick={toggleDarkMode}>{darkTheme ? "Dark" : "Light"}</div>;
+  return (
+    <>
+      <input
+        type="checkbox"
+        id="themeToggle"
+        onChange={toggleDarkMode}
+        checked={darkTheme}
+        className={styles.input}
+      />
+      <label htmlFor="themeToggle" className={styles.label}>
+        {darkTheme ? "Switch to light theme" : "Switch to dark theme"}
+      </label>
+    </>
+  );
 }
