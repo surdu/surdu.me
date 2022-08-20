878146c57b8e67a8c56d6b291d9da2f100ba4849
Surdu <neon-doubt@maskmail.net>
2022-08-20 17:16:07 +0300

    Completed theme switcher

--- components/ThemeToggle/ThemeToggle.tsx
+++ components/ThemeToggle/ThemeToggle.tsx
@@ -1,5 +1,11 @@
 import useDarkTheme from "~/components/ThemeToggle/useDarkTheme";
 
+import Hills from "./hills.svg";
+import Sun from "./sun.svg";
+import Moon from "./moon.svg";
+import Stars from "./stars.svg";
+import Clouds from "./clouds.svg";
+
 import styles from "./ThemeToggle.module.scss";
 
 export default function ThemeToggle() {
@@ -15,7 +21,14 @@ export default function ThemeToggle() {
         className={styles.input}
       />
       <label htmlFor="themeToggle" className={styles.label}>
-        {darkTheme ? "Switch to light theme" : "Switch to dark theme"}
+        Use dark theme
+        <div aria-hidden="true" className={styles.landscape}>
+          <Sun className={styles.sun} />
+          <Clouds className={styles.clouds} />
+          <Moon className={styles.moon} />
+          <Stars className={styles.stars} />
+          <Hills className={styles.hills} />
+        </div>
       </label>
     </>
   );
