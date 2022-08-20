4d61cbe8286104e9cddd3ea786d1a21f839dccb3
Surdu <neon-doubt@maskmail.net>
2022-08-20 23:53:56 +0300

    Added legal pages

--- components/layout/Footer/Footer.tsx
+++ components/layout/Footer/Footer.tsx
@@ -1,12 +1,24 @@
+import Link from "next/link";
 import styles from "./Footer.module.scss";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      &copy;
-      <span className={styles.blue}>&nbsp;{new Date().getFullYear()} </span>
-      <span className={styles.yellow}>&nbsp;Nicu </span>
-      <span className={styles.red}>&nbsp;Surdu </span>
+      <div>
+        &copy;
+        <span className={styles.blue}>&nbsp;{new Date().getFullYear()} </span>
+        <span className={styles.yellow}>&nbsp;Nicu </span>
+        <span className={styles.red}>&nbsp;Surdu </span>
+      </div>
+      <div className={styles.legal}>
+        <Link href="/legal/terms">
+          <a>Terms and Conditions</a>
+        </Link>
+        &bull;
+        <Link href="/legal/disclaimer">
+          <a>Disclaimer</a>
+        </Link>
+      </div>
     </footer>
   );
 }
