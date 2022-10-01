c5299272a636d29194326fe3dd16b5bd88ca8c3e
Surdu <neon-doubt@maskmail.net>
2022-10-01 22:20:24 +0300

    Added tweet button

---
+++ components/TweetButton/TweetButton.tsx
@@ -0,0 +1,30 @@
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+
+import styles from "./TweetButton.module.scss";
+
+interface TweetButtonProps {
+	text: string;
+	children: string;
+	url?: string;
+}
+
+export default function TweetButton(props: TweetButtonProps) {
+	const { text, children, url } = props;
+
+	let finalText = text;
+
+	if (url) {
+		finalText += `%0A%0A${encodeURIComponent(url)}`;
+	}
+
+	return (
+		<Link href={`https://twitter.com/intent/tweet?text=${finalText}`}>
+			<a rel="noreferrer noopener" target="_blank" className={styles.button}>
+				<FontAwesomeIcon icon={faTwitter} className={styles.icon} />
+				<span>{children}</span>
+			</a>
+		</Link>
+	);
+}
--- pages/[year]/[month]/[day]/[page].tsx
+++ pages/[year]/[month]/[day]/[page].tsx
@@ -12,38 +12,48 @@ import BackToTop from "~/components/BackToTop/BackToTop";
 import Meta from "~/components/Meta";
 
 import styles from "./BlogPage.module.scss";
+import TweetButton from "~/components/TweetButton/TweetButton";
+import config from "~/lib/config";
 
 interface BlogPostsProps {
-  post: Post;
+	post: Post;
 }
 
 export default function BlogPosts({ post }: BlogPostsProps) {
-  const rawDate = new Date(post.date);
-  const date = new Intl.DateTimeFormat("en-uk", { dateStyle: "medium" }).format(
-    rawDate
-  );
-  const datetime = rawDate.toISOString().split("T")[0];
-  return (
-    <Layout>
-      <Meta title={post.title} post={post} />
+	const rawDate = new Date(post.date);
+	const date = new Intl.DateTimeFormat("en-uk", { dateStyle: "medium" }).format(
+		rawDate
+	);
+	const datetime = rawDate.toISOString().split("T")[0];
+	return (
+		<Layout>
+			<Meta title={post.title} post={post} />
 
-      <h1 className={styles.title}>{post.title}</h1>
-      <time
-        className={styles.date}
-        dateTime={datetime}
-        aria-label="Publish date"
-      >
-        {date}
-      </time>
-      <article className={styles.article}>
-        <Markdown>{post.markdown}</Markdown>
-      </article>
-      <div className={styles.comments}>
-        <Comments post={post} />
-      </div>
-      <BackToTop />
-    </Layout>
-  );
+			<h1 className={styles.title}>{post.title}</h1>
+			<time
+				className={styles.date}
+				dateTime={datetime}
+				aria-label="Publish date"
+			>
+				{date}
+			</time>
+			<article className={styles.article}>
+				<Markdown>{post.markdown}</Markdown>
+			</article>
+			<div className={styles.shareBar}>
+				<TweetButton
+					text={`I just read "${post.title}" by @surdume`}
+					url={`https://${config.domain}/${post.url}`}
+				>
+					Tweet this article
+				</TweetButton>
+			</div>
+			<div className={styles.comments}>
+				<Comments post={post} />
+			</div>
+			<BackToTop />
+		</Layout>
+	);
 }
 
 interface Params {
