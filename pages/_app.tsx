d1c76852b9cb2c9f47236943707edb2b42a24cc1
surdu <nicu@surdu.me>
2023-08-16 18:20:20 +0300

    Improvements to theme switcher
    Decreased chance of no-flash not executing before the CSS is applied

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,10 @@
-import "~/styles/theme.scss";
 import "~/styles/globals.scss";
 import "~/styles/syntax-light.scss";
 import "~/styles/syntax-dark.scss";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,29 +2,27 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;1,400&family=Rubik:wght@500&display=swap"
-          rel="stylesheet"
-        />
-        <script
-          defer
-          src="https://app.tinyanalytics.io/pixel/VwgdUjyKKvluXFEK"
-        ></script>
-      </Head>
-      <body>
-        <script src="/no-flash.js" />
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang="en">
+			<Head>
+				<link href="/no-flash.js" rel="preload" as="script" />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin="anonymous"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;1,400&family=Rubik:wght@500&display=swap"
+					rel="stylesheet"
+				/>
+				<script src="https://app.tinyanalytics.io/pixel/VwgdUjyKKvluXFEK"></script>
+			</Head>
+			<body>
+				<script src="/no-flash.js" />
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
